c5924b530f662bea27ebbb56983d5e81b339105a
KeertiSaikrishna <31760338+KeertiSaikrishna@users.noreply.github.com>
2018-09-05 19:02:55 +0530

    Add files via upload

---
+++ FormatMain.java
@@ -0,0 +1,8 @@
+public class FormatMain {
+    public static void main(String[] args) {
+        Solution demo = new Solution();
+        int[] arr = new int[]{4, 35, 80, 123, 12345, 44, 8, 5};
+
+        demo.solution(arr,8);
+    }
+}
---
+++ Solution.java
@@ -0,0 +1,72 @@
+import javax.sound.midi.Soundbank;
+
+public class Solution {
+    public void solution(int[] arr, int k) {
+        int max = 0;
+        for (int i : arr) {
+            if (i > max)
+                max = i;
+        }
+        String biggest_element = Integer.toString(max);
+        int max_length = biggest_element.length();
+        int no_rows = max_length / k == 0 ? (max_length / k) : (max_length / k + 1);
+        int no_lines;
+        if (max_length <= k) {
+            no_lines = 3;
+            k = arr.length;
+        } else
+            no_lines = (no_rows * 2) + 1;
+        int curr_index = 0;
+        for (int l = 1; l <= (no_lines); l++) {
+            if ((l % 2) == 1) {
+                for (int i = 0; i < k; i++) {
+                    System.out.print("+");
+                    for (int j = 0; j < max_length; j++) {
+                        System.out.print("-");
+                    }
+                }
+                System.out.print("+\n");
+            } else {
+                for (int i = 0; i < k; i++) {
+                    System.out.print("|");
+                    String exp = Integer.toString(arr[curr_index]);
+                    int length = exp.length();
+                    int gap = max_length - length;
+                    for (int j = 0; j < gap; j++) {
+                        System.out.print(" ");
+                    }
+                    System.out.print(arr[curr_index++]);
+                    if(curr_index == arr.length){
+                        //System.out.print("|\n");
+                        //System.out.print("+");
+                        break;
+                    }
+                }
+                System.out.print("|\n");
+                if(curr_index == arr.length){
+                    if( arr.length % k != 0) {
+                        int noEle_lastRow = arr.length % k;
+                        for (int i = 0; i < noEle_lastRow; i++) {
+                            System.out.print("+");
+                            for (int j = 0; j < max_length; j++) {
+                                System.out.print("-");
+                            }
+                        }
+                        System.out.print("+");
+                        break;
+                    }
+                    else {
+                        for (int i = 0; i < k; i++) {
+                            System.out.print("+");
+                            for (int j = 0; j < max_length; j++) {
+                                System.out.print("-");
+                            }
+                        }
+                        System.out.print("+\n");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
