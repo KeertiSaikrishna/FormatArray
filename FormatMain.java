bd9875387afc3585dbe0274d50f619a19874bc97
KeertiSaikrishna <31760338+KeertiSaikrishna@users.noreply.github.com>
2018-09-07 12:45:24 +0530

    Delete FormatMain.java

--- FormatMain.java
+++
@@ -1,8 +0,0 @@
-public class FormatMain {
-    public static void main(String[] args) {
-        Solution demo = new Solution();
-        int[] arr = new int[]{4, 35, 80, 123, 12345, 44, 8, 5, 23, 43, 12, 9};
-
-        demo.solution(arr, 4);
-    }
-}
